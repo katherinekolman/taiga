4fdb4a6bbf56aa0300085b4bd3ca58f32405f882
unknown <kkolman@wisc.edu>
2020-08-31 17:45:58 -0400

    Debug heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import data from './data';
 import config from './config';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import bodyParser from 'body-parser';
 
-dotenv.config();
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
     useNewUrlParser: true,
