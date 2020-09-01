85aa0baf1a155d74f435f1de944c1f6b38c37e81
unknown <kkolman@wisc.edu>
2020-09-01 12:59:12 -0400

    Change to use db on cloud

--- backend/config.js
+++ backend/config.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/taiga',
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://admin:admin@cluster0.xhxi3.mongodb.net/taigadb?retryWrites=true&w=majority',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
